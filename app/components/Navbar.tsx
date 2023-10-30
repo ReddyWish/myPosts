d15e2c931b65175001bb373414b8c475e8d0d0f3
Ilya <ilya.shuvatov1@gmail.com>
2023-10-30 09:47:10 +0100

    align changes

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 export default function Navbar() {
   return (
     <nav className='bg-zinc-950 p-4 sticky top-0 drop-shadow-xl z-10'>
-      <div className='md:px-6 prose prose-xl mx-auto flex justify-between flex-col sm:flex-row'>
+      <div className='md:px-6 prose prose-xl mx-auto flex justify-between items-center flex-col sm:flex-row'>
         <Link href='/' className='text-white/90 no-underline hover:text-white'>R&W</Link>
         <div className='flex flex-row justify-center sm:justify-evenly align-middle gap-4 text-white text-2xl lg:text-3xl'>
           <Link className='text-white/90 hover:text-white' href='https://github.com/ReddyWish'>
